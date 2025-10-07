6c833b6ac6350d7416554b2446eaafc9c609d974
ZenithCobra <baillie.ace@gmail.com>
2025-10-07 16:17:00 +0000

    edits

--- run_script.py
+++ run_script.py
@@ -1,6 +1,7 @@
 import importlib
 import script
 import NHL_script
+import NHL_data_fetcher
 
 # Reload the script after making changes
 importlib.reload(script)
@@ -14,6 +15,10 @@ print('script getting schedule')
 # SCHEDULE INFO
 script.update_picture()
 
-NHL_script.process_schedule()
+NHL_script.make_todays_schedule()
 NHL_script.process_yesterdays_scores_to_report()
-# NHL_script.process_raw_skaters_html_table()
+NHL_data_fetcher.get_nhl_skaters_2024_2025_csv()
+NHL_script.process_raw_skaters_2024_2025_html_table()
+NHL_script.process_raw_goalies_2024_2025_html_table()
+NHL_script.process_raw_lines_2024_2025_html_table()
+NHL_script.process_raw_teams_2024_2025_html_table()
