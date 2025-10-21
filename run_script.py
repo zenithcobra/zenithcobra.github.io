aa9c125804e94619e2ee02e91b9ec0364a54da8c
ZenithCobra <baillie.ace@gmail.com>
2025-10-21 21:13:10 +0000

    fly edits

--- run_script.py
+++ run_script.py
@@ -1,25 +1,4 @@
-import importlib
-import script
 import NHL_script
-
-# Reload the script after making changes
-importlib.reload(script)
-
-# RUNNING THE SCRIPT
-print("script getting date")
-
-# DATES
-date = script.get_date()
-
-print("script getting schedule")
-# SCHEDULE INFO
-script.update_picture()
-
-# NHL SCHEDULE
-NHL_script.make_todays_schedule()
-
-# NHL YESTERDAYS SCORES FROM NHL_API
-# NHL_script.process_yesterdays_scores_to_report()
 
 # NHL DATA SCRAPE FROM MONEYPUCK
 NHL_script.get_nhl_skaters()
--- run_script2.py
+++ run_script2.py
@@ -1,23 +1,14 @@
-import importlib
 import script
 import NHL_script
-import NHL_data
-
-# Reload the script after making changes
-importlib.reload(script)
 
 # RUNNING THE SCRIPT
-print("script getting date")
-
-# DATES
-date = script.get_date()
-
 print("script getting schedule")
-# SCHEDULE INFO
+
+# MLB SCHEDULE INFO
 script.update_picture()
 
 # NHL SCHEDULE
 NHL_script.make_todays_schedule()
 
-# NHL YESTERDAYS SCORES FROM NHL_API
-# NHL_script.process_yesterdays_scores_to_report()
+# NHL yesterdays scoring report
+NHL_script.process_yesterdays_scores_to_report()
