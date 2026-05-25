3e6aac619fc4272e20d1bb1d27711a97c8f89401
ZenithCobra <baillie.ace@gmail.com>
2026-05-25 15:18:44 +0000

    edits

---
+++ script11_get_team_history.py
@@ -0,0 +1,61 @@
+import os
+import json
+from bs4 import BeautifulSoup
+import statsapi
+from datetime import datetime, timedelta
+
+# File paths
+json_file_path = "text_output/teams_playing_today.json"
+
+if not os.path.exists(json_file_path):
+    raise FileNotFoundError(f"{json_file_path} does not exist.")
+
+# Read the JSON file into a list
+def read_json_to_list(file_path):
+    with open(file_path, "r", encoding="utf-8") as file:
+        data = json.load(file)
+    
+    if not isinstance(data, list):
+        raise ValueError("The JSON file does not contain a list.")
+    
+    return data
+
+teams_playing_today = read_json_to_list(json_file_path)
+
+team_ids = []
+for team in teams_playing_today:
+    # print(team)
+    team_ids.append(statsapi.lookup_team(team)[0].get('id'))
+
+# print(team_ids)
+
+# get the proper formatted date
+# mlb_date = (datetime.now() - timedelta(days=1)).strftime("%Y-%m-%d")
+mlb_date = (datetime.now() - timedelta(days=1)).strftime("%m/%d/%Y")
+# mlb_date = datetime.now().strftime("%m/%d/%Y")
+
+# new_team_list = []
+team_dict = {}
+for a in team_ids:
+    # how to get the last 15 games for a team
+    sched = statsapi.schedule(start_date='01/01/2025',end_date=mlb_date,team=a)
+    newlist = sorted(sched, key = lambda k: k["game_date"], reverse=True)
+    game_data_list = []
+    for game in newlist[0:20]:
+    #     # beans = statsapi.get("game", {"gamePk": game.get('game_id')})
+    #     beans = statsapi.get("game", {"gamePk": game.get('gamePk')})
+        game_data_list.append(game.get('game_id'))
+        # print(f"{a} -> {}")
+    team_dict.update({a:game_data_list})
+    # new_team_list.append(team_dict)
+
+# save the new team list to a JSON file
+output_json_path = "text_output/teams_last_10_games.json"
+with open(output_json_path, "w", encoding="utf-8") as output_file:
+    json.dump(team_dict, output_file, indent=4)
+print(f"Teams' last 10 games saved to {output_json_path}")
+
+
+
+# for x in new_team_list:
+#     print(x)
---
+++ script4_todays_schedule.py
@@ -0,0 +1,67 @@
+# %%
+# GET TODAYS SCHEDULE
+
+import statsapi
+from datetime import datetime, timedelta
+import os
+import pytz
+
+# Ensure the "text_output" folder exists
+os.makedirs("text_output", exist_ok=True)
+
+# Get yesterday's date
+yesterday = (datetime.now() - timedelta(days=1)).strftime("%Y-%m-%d")
+
+# File paths
+file_name = "todays_schedule.txt"
+report_file_path = f"text_output/{file_name}"
+
+# Get today's date
+mlb_date = datetime.now().strftime("%m/%d/%Y")
+schedule = statsapi.schedule(start_date=mlb_date, end_date=mlb_date)
+
+# -----------------------------------
+for x in schedule:
+    # Example UTC datetime string
+    utc_datetime_str = x.get("game_datetime")
+
+    # Parse the UTC datetime string
+    utc_datetime = datetime.strptime(utc_datetime_str, '%Y-%m-%dT%H:%M:%SZ')
+
+    # Define the UTC and Eastern Time zones
+    utc_zone = pytz.utc
+    eastern_zone = pytz.timezone('US/Eastern')
+
+    # Localize the datetime to UTC
+    utc_datetime = utc_zone.localize(utc_datetime)
+
+    # Convert to Eastern Time
+    eastern_datetime = utc_datetime.astimezone(eastern_zone)
+
+    # Format the datetime in a readable format
+    readable_format = eastern_datetime.strftime('%Y-%m-%d %I:%M %p %Z')
+
+    beans = x.get('summary').split('-')
+    new_beans = readable_format + " - " + beans[3].strip()
+    x.update({"summary": new_beans})
+# -----------------------------------
+
+
+today_schedule_content = "Today's Schedule:\n" + "\n".join(
+    f'{x.get("summary")}' for x in schedule
+)
+# ----------------------------------
+
+# Combine all content
+full_content = (
+    today_schedule_content
+)
+
+# Write content to the new Todays_Report.txt file
+with open(report_file_path, "w") as file:
+    file.write(full_content)
+
+print(f"New report saved to {report_file_path}")
+
+
+
